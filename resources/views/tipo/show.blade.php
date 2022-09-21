692863a903cda07256c9f6830c5a1786d4fb5986
ISMAEL HURTADO MARQUES <ismael.hmarques@senacsp.edu.br>
2022-09-21 20:00:20 -0300

    first commit

---
+++ resources/views/tipo/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+@section('conteudo')
+    <h1>Tipo: </h1>
+    <ol>
+        <li>....</li>
+    </ol>
+@endsection
+
+git remote add origin git@github.com:thomasmelo/fluxo-de-caixa.git
